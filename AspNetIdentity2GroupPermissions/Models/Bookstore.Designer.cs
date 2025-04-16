96e4033aaf578434e60e7c57b76428b98eccc9fb
Yaroslav Tsalko <yaroslav_tsalko@epam.com>
2025-04-16 12:22:38 +0300

    Publish commit

--- AspNetIdentity2GroupPermissions/Models/Bookstore.Designer.cs
+++ AspNetIdentity2GroupPermissions/Models/Bookstore.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'C:\Users\Administrator\Desktop\AspNetIdentity2GroupPermissions-master\AspNetIdentity2GroupPermissions\Models\Bookstore.edmx'. 
+﻿// T4 code generation is enabled for model 'C:\Users\yaroslav_tsalko\Projects\test\Bookstore\AspNetIdentity2GroupPermissions\Models\Bookstore.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
